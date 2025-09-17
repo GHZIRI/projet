241f29791f0f0f1e490be538c73bcca4733457ce
salman <tangersalman14@gmail.com>
2025-09-17 15:09:12 +0100

    ch8

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-let nombre1 = 10;
-let nomber2 = 5;
-let somme = nombre1 + nomber2
-console.log("la somme est :" + somme )
-
-function addition(a, b) {
+function additon(a, b){
     return a + b;
 
 }
-let somme1 = addition(8, 5);
-console.log("la somme est :" + somme1);
+let somme = additon(8, 5);
+console.log("la somme est :" + somme);
