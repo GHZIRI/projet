289b6647459e38e036ee6826eae4e5a71268bd50
salman <tangersalman14@gmail.com>
2025-09-17 13:32:41 +0100

    modi 17

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ let nombre1 = 10;
 let nomber2 = 5;
 let somme = nombre1 + nomber2
 console.log("la somme est :" + somme )
+
+function addition(a, b) {
+    return a + b;
+
+}
+let somme1 = addition(8, 5);
+console.log("la somme est :" + somme1);
